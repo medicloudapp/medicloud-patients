dcf3467c363072084ad0bc4c994055334bc711d5
WILLIDEVELOPER <wilsai1023@gmail.com>
2025-03-18 07:38:06 -0500

    ssl v10

--- modules/auth/services/auth.ts
+++ modules/auth/services/auth.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { Agent } from 'https';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: { "Content-Type": "application/json" },
+  httpsAgent: new Agent({
+    rejectUnauthorized: false,
+  }),
 });
 
 export const UserLogin = async (document: string, password: string) => {
