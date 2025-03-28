77ff9433a99295201f75aee28eba621976efe9e1
WILLIDEVELOPER <wilsai1023@gmail.com>
2025-03-28 14:38:17 -0500

    refactor(results-form): convert component to client-side rendering
    Migrate ResultsForm from server-side to client-side rendering using React hooks. This change improves performance by reducing server load and enhances user experience with dynamic data fetching. The component now utilizes the useSession hook for session management and useEffect for data fetching.

--- modules/results/components/results-form.tsx
+++ modules/results/components/results-form.tsx
@@ -1,25 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { auth } from "@/auth";
 import { Heading } from "@/components/ui/heading";
 import { getAttachmentsByPatientId } from "@/modules/results/services/get-attachments";
 import { ResultsTable } from "@/modules/results/components/results-table";
+import { useSession } from "next-auth/react";
+import { Attach } from "@/modules/results/interfaces/Attach";
 
-export const ResultsForm = async () => {
-  const session = await auth();
-  const initialData = await getAttachmentsByPatientId(
-    session?.user?.id as string
-  );
+export const ResultsForm = () => {
+  const { data: session } = useSession();
+  const [initialData, setInitialData] = useState<Attach[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (session?.user?.id) {
+        const data = await getAttachmentsByPatientId(session.user.id);
+        setInitialData(data);
+      }
+    };
+
+    fetchData();
+  }, [session]);
+
+  if (!session?.user) return null;
 
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
           title="Resultados de Pruebas"
-          description={`Bienvenido, ${session?.user?.name}!!`}
+          description={`Bienvenido, ${session.user.name}!!`}
         />
       </div>
       <ResultsTable
         initialData={initialData}
-        token={session?.user?.token as string}
+        token={session.user.token as string}
       />
     </>
   );
